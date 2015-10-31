1a01d13722aa72b591c213b2fa67ae342b95f2b4
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-30 22:10:21 -0700

    Test pass get :show

--- app/controllers/api/v1/children_controller.rb
+++ app/controllers/api/v1/children_controller.rb
@@ -3,4 +3,19 @@ class API::V1::ChildrenController < ApplicationController
     childrens = Children.all
     render json: childrens.to_json, status: 200
   end
+
+  def show
+    @children = Children.find_by(id: params[:id])
+
+    if @children
+      render json: { success: true,
+                     first_name: @children.first_name,
+                     last_name: @children.last_name,
+                     age: @children.age,
+                     date_of_birth: @children.date_of_birth
+      }, status: :ok
+    else
+      render json: { success: false }, status: :unprocessable_entity
+    end
+  end
 end
--- spec/api/v1/controllers/children_controller_spec.rb
+++ spec/api/v1/controllers/children_controller_spec.rb
@@ -3,11 +3,42 @@ require 'rails_helper'
 RSpec.describe API::V1::ChildrenController, type: :controller do
   context "GET #index" do
     it "returns http status of 200" do
-      children = Children.create!(first_name: "Joe", last_name: "Smith", age: "18", date_of_birth: DateTime.new(1997))
+      children = Children.create!(first_name: "Joe", last_name: "Smith", age: 18, date_of_birth: DateTime.new(1997))
 
       get :index
       expect(response.status).to eq(200)
       expect(response.content_type).to eq('application/json')
+
+      response_message = JSON.parse(response.body)
+      expect(response_message.length).to eq(1)
+    end
+  end
+
+  context "GET children/id" do
+    it "valid id" do
+      children = Children.create!(first_name: "Joe", last_name: "Smith", age: 18, date_of_birth: DateTime.new(1997))
+
+      get :show, id: children
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq('application/json')
+
+      response_message = JSON.parse(response.body)
+      expect(response_message["success"]).to eq(true)
+      expect(children.first_name).to eq(response_message["first_name"])
+      expect(children.last_name).to eq(response_message["last_name"])
+      expect(children.age).to eq(response_message["age"])
+      expect(children.date_of_birth).to eq(response_message["date_of_birth"])
+    end
+
+    it "invalid id" do
+      children = Children.create!(first_name: "Joe", last_name: "Smith", age: 18, date_of_birth: DateTime.new(1997))
+
+      get :show, id: 99
+      expect(response.status).to eq(422)
+      expect(response.content_type).to eq('application/json')
+
+      response_message = JSON.parse(response.body)
+      expect(response_message["success"]).to eq(false)
     end
   end
 end
