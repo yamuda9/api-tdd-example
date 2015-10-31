3986d492668a128ae185be170e419f15e6a33d9d
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-30 21:31:05 -0700

    Pass render as application/json

--- app/controllers/api/v1/children_controller.rb
+++ app/controllers/api/v1/children_controller.rb
@@ -1,4 +1,6 @@
 class API::V1::ChildrenController < ApplicationController
   def index
+    childrens = Children.all
+    render json: childrens.to_json, status: 200
   end
 end
--- spec/api/v1/controllers/children_controller_spec.rb
+++ spec/api/v1/controllers/children_controller_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe API::V1::ChildrenController, type: :controller do
       children = Children.create!(first_name: "Joe", last_name: "Smith", age: "18", date_of_birth: DateTime.new(1997))
 
       get :index
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq('application/json')
     end
   end
 end
